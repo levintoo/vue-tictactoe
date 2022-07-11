e45eb9c240176db456b60eb15f31e0ff7801c129
levintoo <tooklevin@gmail.com>
2022-07-11 21:55:01 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+
+<script>
+import { ref } from "vue";
+export default {
+  name: 'HomeView',
+  setup() {
+    const count = ref(0);
+    return {
+      count
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{ count }}</h1>
+  <button @click="count++">increment</button>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
