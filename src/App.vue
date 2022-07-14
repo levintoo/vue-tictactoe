0dbaf7da4e2fae745627e6e3325733abb6246090
levintoo <tooklevin@gmail.com>
2022-07-14 13:29:01 +0300

    calculate winner

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,33 @@
+<script setup>
+import { ref, computed } from 'vue'
+const player = ref('X')
+const board = ref([
+  ['', '', ''],
+  ['', '', ''],
+  ['', '', '']
+]);
 
-<script>
-import { ref } from "vue";
-export default {
-  name: 'HomeView',
-  setup() {
-    const count = ref(0);
-    return {
-      count
+const CalculateWinner = (board) => {
+  const lines = [[0, 1, 2],[3, 4, 5],[6, 7, 8],[0, 3, 6],[1, 4, 7],[2, 5, 8],[0, 4, 8],[2, 4, 6]]
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i]
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      return board[a]
     }
   }
+  return null
 }
 </script>
+
 
 <template>
   <h1 class="text-3xl font-bold underline bg-blue">
     Hello world!
   </h1>
-  <h1>{{ count }}</h1>
-  <button @click="count++" class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none">increment</button>
-  
 </template>
 
 <style>
-* {
-  margin: 0;
-  padding: 0;
+body {
+	@apply bg-gray-800 text-white;
 }
 </style>
